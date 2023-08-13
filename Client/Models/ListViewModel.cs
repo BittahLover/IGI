ebf7f9efc8bb719f7c4367d2128d1e22c8d59031
BittahLover <maxx.angelvsdevil@gmail.com>
2023-08-13 17:47:31 +0300

    igi

---
+++ Client/Models/ListViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace WebApplication2.Client.Models
+{
+    public class ListViewModel
+    {
+        [JsonPropertyName("dishId")]
+        public int DishId { get; set; } // id блюда 
+        [JsonPropertyName("dishName")]
+        public string DishName { get; set; } // название блюда 
+
+    }
+
+}
